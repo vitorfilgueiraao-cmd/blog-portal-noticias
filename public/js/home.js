bcf5a3c23ae2b9d5916b7dabf573ff649bce7764
mateusdz7-canno <mateusv2044502@gmail.com>
2026-07-22 12:04:41 -0300

    cards do site

--- public/js/home.js
+++ public/js/home.js
@@ -1,1 +1,32 @@
-    
+const noticias=[
+    {
+        titulo:"Nova tecnologia chega ao mercado",
+        imagem:"https://picsum.photos/300/200?1"
+    },
+    {
+        titulo:"Campeonato começa amanhã",
+        imagem:"https://picsum.photos/300/200?2"
+    },
+    {
+        titulo:"Economia cresce em 2026",
+        imagem:"https://picsum.photos/300/200?3"
+    }
+];
+
+const container=document.getElementById("noticias");
+
+noticias.forEach(noticia=>{
+
+    container.innerHTML+=`
+
+        <div class="card">
+
+            <img src="${noticia.imagem}">
+
+            <h3>${noticia.titulo}</h3>
+
+        </div>
+
+    `;
+
+});    
