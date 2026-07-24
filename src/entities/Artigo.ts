ecc84a12be961c1c77d2d0befb65c25a14d491c6
José Luciano Alves de Brito <lucianofilhoalves123@gmail.com>
2026-07-24 13:57:09 -0300

    construindo artigo

---
+++ src/entities/Artigo.ts
@@ -0,0 +1,11 @@
+class Artigo {
+    private id: number;
+    private titulo: string;
+    private conteudo: string;
+
+    constructor(id: number, titulo: string, conteudo: string) {
+        this.id = id;
+        this.titulo = titulo;
+        this.conteudo = conteudo;
+    }
+}
